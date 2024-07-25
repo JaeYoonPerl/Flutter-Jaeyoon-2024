9989d608564a4393a63ba03ee0b64db5991cc1ba
JaeYoonLee <a7750430@naver.com>
2024-07-25 11:07:22 +0900

    dd

---
+++ 02.다트연습/01.변수.dart
@@ -0,0 +1,13 @@
+// 01. 다트문법 1 : 변수 사용하기
+
+// main 메서드를 가장 먼저 호출한다
+
+// void는 리턴값이 없다는 표시
+void main(List<String> args){
+// var는 할당되는 값으로 형이 결정되는 
+// 자유로운 변수선언형식이다!
+var myHome ="내집이야";
+//출력은 print()
+print(myHome);
+// cs언어는 마지막 세미콜론 필수
+}
